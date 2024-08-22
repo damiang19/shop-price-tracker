677323902a3b7c9ec3577ff1aa0b3e7e0f3f90a1
damiang19 <damiang19@gmail.com>
2024-08-22 12:33:35 +0200

    #2 Full docker compose config with postgres databases

--- shop-price-tracker/price_tracker/src/main/resources/schema.sql
+++ shop-price-tracker/price_tracker/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-CREATE SEQUENCE IF NOT EXISTS public.tracked_product_archive_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1
+CREATE SEQUENCE IF NOT EXISTS public.tracked_product_archive_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
-CREATE SEQUENCE IF NOT EXISTS public.tracked_product_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1
+CREATE SEQUENCE IF NOT EXISTS public.tracked_product_id_seq INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
 
 CREATE TABLE IF NOT EXISTS public.tracked_product
 (
@@ -8,13 +8,9 @@ CREATE TABLE IF NOT EXISTS public.tracked_product
     product_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
     price numeric NOT NULL,
     product_url character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    shop_id bigint NOT NULL,
     created timestamp without time zone,
-    CONSTRAINT tracked_product_pkey PRIMARY KEY (tracked_product_id),
-    CONSTRAINT fk_shop FOREIGN KEY (shop_id)
-        REFERENCES public.shop (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
+    shop_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT tracked_product_pkey PRIMARY KEY (tracked_product_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.tracked_product_archive
