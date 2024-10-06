8ba261899a0f1483ade3c69a9e97b6f60a3a0b60
damiang19 <damiang19@gmail.com>
2024-10-06 18:55:44 +0200

    #6 Add gateway server to project and create base configuration

---
+++ shop-price-tracker/gatewayserver/src/main/java/pl/dgorecki/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,15 @@
+package pl.dgorecki.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
