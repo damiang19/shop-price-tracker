319912c586b4681b035f4943e8dc95934a6367e2
damiang19 <damiang19@gmail.com>
2024-11-10 15:03:12 +0100

    #6 Tests without context

--- shop-price-tracker/scrapper/src/test/java/pl/dgorecki/scrapper/services/ShopServiceTest.java
+++ shop-price-tracker/scrapper/src/test/java/pl/dgorecki/scrapper/services/ShopServiceTest.java
@@ -2,11 +2,11 @@ package pl.dgorecki.scrapper.services;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import pl.dgorecki.scrapper.ScrapperApplication;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import pl.dgorecki.scrapper.controller.payload.ShopData;
 import pl.dgorecki.scrapper.entity.Shop;
 import pl.dgorecki.scrapper.repository.ShopRepository;
@@ -16,11 +16,12 @@ import pl.dgorecki.scrapper.service.impl.UrlValidatorServiceImpl;
 import pl.dgorecki.scrapper.service.mapper.ShopMapper;
 
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = ScrapperApplication.class)
+@ExtendWith(MockitoExtension.class)
 public class ShopServiceTest {
     @Mock
     private ShopRepository shopRepository;
@@ -36,9 +37,39 @@ public class ShopServiceTest {
 
     @Test
     void findAll_should_return_student_list() {
-        when(urlValidatorService.getBaseShopUrl(anyString())).thenReturn("http://");
-        when(shopMapper.toDto(any(Shop.class))).thenReturn(new ShopDTO());
-        when(shopRepository.save(any(Shop.class))).thenReturn(new Shop());
-        ShopDTO persist = shopService.save(new ShopData("tt", "tt", "tt", "tt"));
+        Shop exampleShop = new Shop();
+        exampleShop.setName("example");
+        exampleShop.setShopUrl("www.example.org");
+        exampleShop.setPriceHtmlClass("price");
+        exampleShop.setProductNameHtmlClass("productName");
+
+        ShopDTO shopDto = new ShopDTO();
+        shopDto.setName("example");
+        shopDto.setShopUrl("www.example.org");
+        shopDto.setPriceHtmlClass("price");
+        shopDto.setProductNameHtmlClass("productName");
+
+        when(urlValidatorService.getBaseShopUrl("www.example.org")).thenReturn("www.er.org");
+        when(shopRepository.save(any())).thenReturn(exampleShop);
+        when(shopMapper.toDto(exampleShop)).thenReturn(shopDto);
+        shopService.save(new ShopData("productName", "price", "example", "www.example.org"));
+
+        Mockito.verify(this.urlValidatorService, Mockito.times(1))
+                .getBaseShopUrl("www.example.org");
+        Mockito.verify(this.shopRepository, Mockito.times(1))
+                .save(any());
+        Mockito.verify(this.shopMapper, Mockito.times(1))
+                .toDto(exampleShop);
+
+    }
+
+    @Test
+    void getShops() {
+
+        when(shopRepository.findAllByNameIn(List.of("eo"))).thenReturn(List.of(new Shop()));
+        List<ShopDTO> persist = shopService.getAllByNames(List.of("eo"));
+        System.out.println(persist);
+
+
     }
 }
